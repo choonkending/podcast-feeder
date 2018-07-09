f5ef1119a9d42212a4faa56da9ed570b2acedfea
Ken Ding <aenkil@gmail.com>
2018-07-09 23:00:36 +1000

    WIP Attempt to use http instead of simpleHttp

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,16 @@
 module Main where
 import Network.HTTP.Conduit
+import Control.Monad.Trans.Resource
 
 main :: IO ()
 main = do
-  response <- simpleHttp "http://feeds.feedburner.com/seriouseats/recipes"
+  -- parseRequest "http://feeds.feedburner.com/seriouseats/recipes" >>=
+  --   \req -> newManager tlsManagerSettings >>=
+  --       \m -> http req m >>=
+  --         \res -> putStrLn show (responseBody res)
+  request <- parseRequest "http://feeds.feedburner.com/seriouseats/recipes"
+  manager <- newManager tlsManagerSettings
+  response <- http request manager
   putStrLn (show response)
+
+
