1fba332c912aa554e7aed183285f86ee439b5e84
ken-ding <ken.ding@rea-group.com>
2018-07-11 16:10:21 +1000

    WIP Add conduit things

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,10 @@
 module Main where
+import Conduit (runConduit, (.|))
+import Data.Conduit.List (sinkNull)
 import Network.HTTP.Conduit
-import Control.Monad.Trans.Resource
+import Control.Monad.Trans.Resource (runResourceT, ResourceT)
+import Text.XML
+import Text.XML.Stream.Parse
 
 main :: IO ()
 main = do
@@ -8,9 +12,15 @@ main = do
   --   \req -> newManager tlsManagerSettings >>=
   --       \m -> http req m >>=
   --         \res -> putStrLn show (responseBody res)
+  --       runConduit $ responseBody r will not work as the types do not match
+  --       We need to compose it with a function that accepts an input of bytestring and output of void
   request <- parseRequest "http://feeds.feedburner.com/seriouseats/recipes"
   manager <- newManager tlsManagerSettings
-  response <- http request manager
-  putStrLn (show response)
+  doc <- runResourceT go
+  print doc
 
+go :: ResourceT IO Document
+go = do
+  r <- (http request manager)
+  runConduit $ responseBody r .| parseBytes def .| fromEvents
 
