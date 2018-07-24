c73238d7907f99d9eea8f3a223f90e50f1eb564e
ken-ding <ken.ding@rea-group.com>
2018-07-24 22:07:58 +1000

    Refactor code to work with >>= syntax
    I've recombined the previous function which worked to main, so that I
    could understand it myself. The types were previously working but I had
    no real understanding of what was happening. Not having a grasp on do
    syntax made it worse, as I did not understand why the types were
    failing.
    
    In following commits, I will tease apart the functions again

--- src/Main.hs
+++ src/Main.hs
@@ -9,20 +9,25 @@ import Text.XML
 import Text.XML.Stream.Parse
 
 main :: IO ()
-main = do
-  -- parseRequest "http://feeds.feedburner.com/seriouseats/recipes" >>=
-  --   \req -> newManager tlsManagerSettings >>=
-  --       \m -> http req m >>=
-  --         \res -> putStrLn show (responseBody res)
-  --       runConduit $ responseBody r will not work as the types do not match
-  --       We need to compose it with a function that accepts an input of bytestring and output of void
-  request <- parseRequest "http://feeds.feedburner.com/seriouseats/recipes"
-  doc <- runResourceT $ go request
-  print doc
+main =
+  -- request <- parseRequest "http://feeds.feedburner.com/seriouseats/recipes"
+  -- doc <- runResourceT $ go request
+  -- print doc
+  createRequest >>= (\request
+    -> runResourceT (getManager >>= (\manager ->
+      (http request manager) >>= (\response ->
+        runConduit $ responseBody response .| parseBytes def .| CL.map (\e -> (Nothing, e)) .| fromEvents
+      )))) >>= \doc -> print doc
 
-go :: Request -> ResourceT IO Document
-go request = do
-  manager <- lift (newManager tlsManagerSettings)
-  r <- (http request manager)
-  runConduit $ responseBody r .| parseBytes def .| CL.map (\e -> (Nothing, e)) .| fromEvents
+getManager :: ResourceT IO Manager
+getManager = lift $ newManager tlsManagerSettings
+
+createRequest :: IO Request
+createRequest = parseRequest "http://feeds.feedburner.com/seriouseats/recipes"
+
+-- go :: Request -> ResourceT IO Document
+-- go request = do
+--   manager <- lift (newManager tlsManagerSettings)
+--   r <- (http request manager)
+--   runConduit $ responseBody r .| parseBytes def .| CL.map (\e -> (Nothing, e)) .| fromEvents
 
