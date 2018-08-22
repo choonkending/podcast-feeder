f4245316689af6699e92161f0998bbec1f5c5a22
ken-ding <ken.ding@rea-group.com>
2018-08-22 16:01:42 +1000

    Run server

--- src/Main.hs
+++ src/Main.hs
@@ -11,13 +11,22 @@ import Control.Monad.Trans.Class (lift)
 import Text.XML.Unresolved (sinkDoc)
 import qualified Data.XML.Types as XT
 import qualified Text.XML.Stream.Parse as SP
-import Servant.API ((:>), QueryParam, Get, JSON)
-import Servant.Server(Server)
+import Servant.API ((:>), QueryParam, Get, JSON, FromHttpApiData(..))
+import Servant.Server (Server, serve)
 import Data.Aeson (ToJSON(..))
 import GHC.Generics (Generic)
+import Network.Wai (Application)
+import Network.Wai.Handler.Warp (run)
+import Data.Proxy (Proxy(..))
 
 main :: IO ()
-main =
+main = run 8081 app where
+  app = serve proxy server
+  proxy :: Proxy RecipeAPI
+  proxy = Proxy
+
+parseFeed :: IO ()
+parseFeed =
   createRequest >>= (\request ->
     runResourceT (getManager >>= (\manager ->
       (http request manager) >>= (\response ->
@@ -41,6 +50,10 @@ recipes1 =
 
 server :: Server RecipeAPI
 server _ = return recipes1
+
+
+instance FromHttpApiData SortBy where
+  parseQueryParam _ = Right PublishedDescending
 
 transformToDocument :: MonadThrow m => ConduitM i B.ByteString m () -> ConduitM i o m XT.Document
 transformToDocument input = input .| sinkDoc SP.def
