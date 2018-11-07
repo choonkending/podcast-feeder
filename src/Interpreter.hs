807ee6da143b3e9b89cb4f847bc7c56803682238
ken-ding <ken.ding@rea-group.com>
2018-11-07 15:49:28 +1100

    Added interpret case for Action

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -5,3 +5,4 @@ import qualified Action as A
 interpret :: A.Action a -> IO a
 interpret (A.StorePosition uid progress) = pure ()
 interpret (A.FetchPosition uid url) = pure Nothing
+interpret (A.Action fa2b aa) = fa2b <$> interpret aa
