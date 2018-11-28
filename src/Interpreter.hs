97f645098e9c45a96b45be879d6b694deaa06380
ken-ding <ken.ding@rea-group.com>
2018-11-28 15:59:44 +1100

    WIP updateUserStore

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -21,7 +21,14 @@ findProgress url (UserStore _ progresses) = find (isProgress url) progresses
 isProgress :: A.Url -> A.Progress -> Bool
 isProgress givenUrl (A.Progress { A.url = url }) = givenUrl == url
 
+storeProgress :: A.Progress -> UserStore -> UserStore
+-- We could remove the duplicate old progress but findProgress only matches on the first progress, so that's a perf optimisation!
+storeProgress progress (UserStore uid progresses) = UserStore uid (progress : progresses)
+
+updateUserStore :: A.UserID -> (UserStore -> UserStore) -> Store -> Store
+updateUserStore uid f store = Store (findUser uid : userStores)
+
 interpret :: Store -> A.Action a -> IO a
-interpret store (A.StorePosition uid progress) = pure ()
+interpret store (A.StorePosition uid progress) = pure (storeProgress progress <$> findUser uid store)
 interpret store (A.FetchPosition uid url) = pure (findUser uid store >>= findProgress url)
 interpret store (A.Action fa2b aa) = fa2b <$> interpret store aa
